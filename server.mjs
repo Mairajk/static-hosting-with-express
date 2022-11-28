277a62073b9fa6f8d7f16a3d7bbd2fa8c3795cf7
Mairajk <mairajkhan597@gmail.com>
2022-11-29 04:20:12 +0500

    create react app

---
+++ server.mjs
@@ -0,0 +1,38 @@
+import exprss from "express";
+import cors from "cors";
+import path from "path";
+
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+
+app.use = (cors());
+
+
+app.get("/hello", (req, res) => {
+    console.log("ip from /hello request : ==> ", req.ip);
+
+    res.send(` Hello World ====>>> ${new Date().toString()} `)
+})
+
+app.get("/weather", (req, res) => {
+    console.log("ip from /weather request : ==>", req.ip);
+
+    res.send(
+        {
+            temp: 28,
+            minTemp: 24,
+            maxTemp: 31,
+            humidity: 76,
+            serverTime: new Date().toString()
+        }
+    )
+})
+
+app.get("/about", (req, res) => {
+    console.log("ip from /about request : ==>", req.ip);
+
+    res.send(` This is About Page ====>>> ${new Date().toString()} `)
+})
