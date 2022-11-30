9a7c29c1d4c9efb128543b3ba164581f8f4f402a
Mairajk <mairajkhan597@gmail.com>
2022-11-30 18:44:47 +0500

    working......1

--- server.mjs
+++ server.mjs
@@ -38,9 +38,13 @@ app.get("/about", (req, res) => {
 })
 
 
+const __dirname = path.resolve();
+
+// app.use("/", express.static(path.join(__dirname, './web/build')));
+// app.use("*", express.static(path.join(__dirname, './web/build/index.html')));
 
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
